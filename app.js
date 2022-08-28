d23bc794e0ed66f1b073bea6ea5b640981e79989
Peter Oodo <pifeanyi2010@gmail.com>
2022-08-28 02:25:18 +0100

    I completed the first part of rock-paper-scissors project of The Odin Project

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const choices = ['rock','paper','scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
+
+const game = () => {
+  for(let i = 1;i <= 5;i++){
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt('enter rock,paper,scissors: ').toLocaleLowerCase();
+    console.log('computer chose: ' + computerSelection );
+    console.log('you chose chose: ' + playerSelection);
+    console.log(playRound(computerSelection,playerSelection));
+    console.log('computer score is : ' + computerScore)
+    console.log('Your score is : ' + playerScore)
+    console.log('-----------------------------')
+  }
+} 
 
 function getComputerChoice(){
-  return choices[Math.floor(Math.random()*choices.length)];
+  return choices[Math.floor(Math.random()*choices.length
+)]
 }
-
 function playRound(computerSelection,playerSelecion) {
   if (computerSelection ===  'paper' && playerSelecion === 'paper') {
     return 'you tie! You both pikced paper'
@@ -16,34 +31,50 @@ function playRound(computerSelection,playerSelecion) {
     return 'you tie! You both pikced scissors'
   }
     else if (playerSelecion ===  'scissors' && computerSelection === 'rock') {
-    return 'You lost, rock crushes scissors'
-  
+    computerScore++
+    return 'Computer won, rock crushes scissors'
+    
   }
     else if (playerSelecion ===  'scissors' && computerSelection === 'paper') {
+    playerScore++
     return 'You won,scissors cuts paper'
   
   }
     else if (playerSelecion ===  'rock' && computerSelection === 'paper') {
-    return 'You lost, paper covers rock'
+    computerScore++
+    return 'Computer won, paper covers rock'
+    
+
   
   }
     else if (playerSelecion ===  'rock' && computerSelection === 'scissors') {
+    playerScore++
     return 'You won, rock crushes scissors'
   
   }
     else if (playerSelecion ===  'paper' &&      computerSelection === 'scissors') {
-    return 'You lost, scissors cuts paper'
+    computerScore++
+    return 'Computer won, scissors cuts paper'
+    
   
   }
     else if (playerSelecion ===  'paper' && computerSelection === 'rock') {
+    playerScore++
     return 'You won,paper covers rock'
-  
   }
 
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log('computer chose: ' + computerSelection);
-console.log('you chose chose: ' + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(game());
+
+const winner = () => {
+  if (computerScore > playerScore){
+    return 'Computer won the game'
+  }else if (computerScore < playerScore){
+    return 'You won the game'
+  }else {
+    return 'Tie'
+  }
+}
+console.log(winner());
